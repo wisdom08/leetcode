d289dab485ab30af67f7886d09df9e82cf4fa811
Byun Jihye <wisdom08@kakao.com>
2022-09-07 21:16:47 +0900

    Time: 0 ms (100.00%), Space: 42.9 MB (82.52%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int result = 0;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] == target || nums[i] > target){
+                result = i;
+                break;
+            }
+            result = i+1;
+        }
+        return result;
+    }
+}
