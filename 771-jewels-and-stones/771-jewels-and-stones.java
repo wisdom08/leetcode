8abc931179ecb05e38db32b6b411be006e62cf2f
Byun Jihye <wisdom08@kakao.com>
2022-09-22 23:34:40 +0900

    Time: 1 ms (98.46%), Space: 40.4 MB (98.25%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.java
+++ 771-jewels-and-stones/771-jewels-and-stones.java
@@ -1,15 +1,11 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        char[] stonesArr = stones.toCharArray();
-        char[] jewelsArr = jewels.toCharArray();
-        int sum = 0;
-        for (char c : stonesArr) {
-            for (char j : jewelsArr) {
-                if (c == j) {
-                    sum++;
-                }
-            }
+         int count = 0;
+
+        for (Character c : stones.toCharArray()) {
+            if(jewels.contains(c.toString())) count++;
         }
-        return sum;
+
+        return count;
     }
 }
